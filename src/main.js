ed74b2d38ac6b12b29f486f16372d064647ad1ba
yukinissie <bigtree2490@outlook.jp>
2022-08-06 12:22:23 +0900

    try grahql clent in vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core'
+import gql from 'graphql-tag'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/',
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+apolloClient
+  .query({
+    query: gql`
+      query GetUsers {
+        users {
+          id
+          name
+          email
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result))
 
 createApp(App).mount('#app')
