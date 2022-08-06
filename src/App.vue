eaa06110e8c227a00ee81f2add49460c3e7a216d
yukinissie <bigtree2490@outlook.jp>
2022-08-06 12:31:54 +0900

    Add users query

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script setup></script>
+<script setup>
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      id
+      name
+      email
+    }
+  }
+`
+
+const { result, loading, error } = useQuery(GET_USERS)
+</script>
 
 <template>
   <h1>GraphQL</h1>
+  <div v-if="loading"><p>ローディング中です</p></div>
+  <div v-else-if="error">
+    <p>エラーが発生しています。</p>
+    <p></p>
+  </div>
+  <div v-else>
+    <div v-for="user in result.users" :key="user.id">Name: {{ user.name }}</div>
+  </div>
 </template>
